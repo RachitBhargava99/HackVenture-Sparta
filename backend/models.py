c69407fcff88fddba86c52c783ce4b5064b072fd
RachitBhargava99 <rachitbhargava99@gmail.com>
2019-01-19 02:55:27 -0500

    Basic Event Management Done

--- backend/models.py
+++ backend/models.py
@@ -9,7 +9,6 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(127), nullable=False)
     email = db.Column(db.String(63), unique=True, nullable=False)
-    password = db.Column(db.String(63), nullable=False)
     isAdmin = db.Column(db.Boolean, nullable=False, default=False)
 
     def get_auth_token(self, expires_seconds=86400):
